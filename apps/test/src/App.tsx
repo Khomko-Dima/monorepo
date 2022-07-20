e639085a9823733d629d78ca5e691cc12bd3edcf
Khomko Dima <37930691+Khomko-Dima@users.noreply.github.com>
2022-07-20 17:39:47 +0300

    Initial commit

---
+++ apps/test/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, Link } from "ui";
+
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Link className="App-link" href="https://reactjs.org">
+          Learn React sd
+        </Link>
+        <Button/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+
+
+import { Link } from "./Link";
+import { Button } from "./components/";
+export { Link, Button }
